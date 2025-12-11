fa53b4274662a075f0e920fbc46a906613add7ca
alaa <alaanedalabdo@gmail.com>
2025-12-11 16:55:57 +0200

    fixed compilation error due to unhandled exception

--- src/main/java/edu/software/lms/CustomUtilities.java
+++ src/main/java/edu/software/lms/CustomUtilities.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.logging.Logger;
 
 public class CustomUtilities {
 
@@ -17,15 +18,17 @@ public class CustomUtilities {
         return hasDigit && hasLower && hasUpper && hasSymbol;
     }
 
-    public static String hashPassword(String password, String username) throws HashingException {
+    public static String hashPassword(String password, String username) {
         try {
             String saltedPassword = password + username;
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             byte[] hashedBytes = md.digest(saltedPassword.getBytes(StandardCharsets.UTF_8));
             return Base64.getEncoder().encodeToString(hashedBytes);
         } catch (NoSuchAlgorithmException e) {
-            throw new HashingException("SHA-256 algorithm not found!", e);
+            Logger.getLogger(CustomUtilities.class.getName()).severe("SHA-256 algorithm not found: " + e);
+            return null;
         }
+
     }
 
     public static class HashingException extends Exception {
